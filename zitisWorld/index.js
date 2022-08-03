16d76b9ecbf80890929ebd466a262686cdc14ffd
Mike Cognata <mikeyintech@gmail.com>
2022-08-03 19:04:50 -0400

    finally got speech sybthesis working

---
+++ zitisWorld/index.js
@@ -0,0 +1,19 @@
+const txt1 = document.getElementById("any-word");
+const btn1 = document.getElementById("btn1");
+const out1 = document.getElementById("output1");
+
+function fun1() {
+    out1.innerHTML = txt1.value; 
+    
+}
+
+btn1.addEventListener("click", fun1);  
+
+btn1.addEventListener("click", () => {
+    playText(txt1.value)
+})
+        
+function playText(text) {
+    const utternace = new SpeechSynthesisUtterance(text)
+    speechSynthesis.speak(utternace)
+}
