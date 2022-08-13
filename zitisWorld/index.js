e7c1195e8b8cf01f098b0c8cbf40a5643f63d8a7
Mike Cognata <mikeyintech@gmail.com>
2022-08-13 17:52:31 -0400

    working on button routes in js

--- zitisWorld/index.js
+++ zitisWorld/index.js
@@ -7,11 +7,26 @@ function fun1() {
     
 }
 
-btn1.addEventListener("click", fun1);  
+btn1.addEventListener("click", fun1);
+
+// btn1.addEventListener("click", () => {
+//     let keyEvent = new KeyboardEvent('keypress', { kay: 'Enter'});
+
+//     document.body.dispatchEvent(keyEvent);
+// });
+
+
 
 btn1.addEventListener("click", () => {
-    playText(txt1.value)
-})
+    
+    playText(txt1.value);
+    txt1.value = '';
+    let keyEvent = new KeyboardEvent('keypress', { kay: 'Enter'});
+
+    document.body.dispatchEvent(keyEvent);
+});
+
+
         
 function playText(text) {
     const utternace = new SpeechSynthesisUtterance(text)
